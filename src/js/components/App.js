17acaf8d7372bc1014fcf3146fbe5ba32193014b
Yang Ke <saint_yk@163.com>
2017-04-15 21:23:47 +0800

    first commit

---
+++ src/js/components/App.js
@@ -0,0 +1,43 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+
+function getAppState() {
+	return {
+
+	}
+}
+
+var App = React.createClass({
+	getInitialState: function() {
+		return getAppState();
+	},
+
+	componentDidMount: function() {
+		AppStore.addChangeListener(this._onChange);
+	},
+
+	componentUnmount: function() {
+		AppStore.removeChangeListener(this._onChange);
+	},
+
+	render: function() {
+		return (
+			<div>
+				<h1 className="text-center page-header">WorkoutLogger</h1>
+				<button className="btn btn-primary btn-block">Add Workout</button>
+				<br />
+				FORM
+				<br />
+				WORKOUTS
+			</div>
+		);
+	},
+
+	// Update view state when change is received
+	_onChange: function() {
+		this.setState(getAppState());
+	}
+});
+
+module.exports = App;
