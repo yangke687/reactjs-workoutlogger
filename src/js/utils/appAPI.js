28c1e29dbde8276f8ecd83dd4b9814d4ca849a8f
Yang Ke <saint_yk@163.com>
2017-04-16 14:24:05 +0800

    save workout in localstorage

--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -2,6 +2,16 @@ var AppActions = require('../actions/AppActions');
 
 module.exports = {
 	addWorkout: function(workout) {
-
+		//console.log('saving workout in localStorage');
+		var workouts = JSON.parse(localStorage.getItem('workouts'));
+		if (!workouts) {
+			workouts = [];
+		}
+		workouts.push(workout);
+		localStorage.setItem('workouts', JSON.stringify(workouts));
+	},
+	getWorkouts: function() {
+		var workouts = JSON.parse(localStorage.getItem('workouts'));
+		AppActions.receiveWorkouts(workouts);
 	}
 }
